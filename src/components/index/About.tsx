02fd7898029b0d644b23388d56ab2b12f4117457
Alexis Aguilar <alexmerona@gmail.com>
2023-05-25 19:53:34 -0400

    feature: add bio info into about section

--- src/components/index/About.tsx
+++ src/components/index/About.tsx
@@ -1,23 +1,37 @@
 export const About = () => {
   return (
     <main className="w-full py-10">
-      <div className="container flex gap-72 px-32">
-        <section className="flex flex-col gap-2">
+      <div className="container flex gap-40 px-32">
+        <section className="flex flex-col gap-2 w-1/2">
           <h2 className="uppercase font-mont font-medium">Before</h2>
           <p className="text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt cumque
-            esse nemo animi adipisci ipsum ut ad praesentium nesciunt,
-            accusantium quaerat error expedita id ipsam! Illo ut quis iusto
-            modi.
+            In my final 2 years at Stetson University, I conducted Herpetology +
+            Invasive Species research while also serving my peers as both a
+            Biology and Writing Teaching Assistant. After receiving a B.S. in
+            both Biology and Chemistry, I was privileged to help my community as
+            a veterinary technician in gold standard animal hospitals for over
+            2.5 years. While managing animal hospitals, I learned full stack web
+            development through the part time, remote bootcamp 100Devs. I built
+            multiple projects in cross-functional teams. I successfully
+            delivered to clients, including a Partnered Twitch streamer's
+            e-commerce website, achieving a quarterly increase of 248% in total
+            sales, 102% in conversion rate, and 100% in returning customer rate.
           </p>
         </section>
-        <section className="flex flex-col gap-2">
+        <section className="flex flex-col gap-2 w-1/2">
           <h2 className="uppercase font-mont font-medium">Now</h2>
           <p className="text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt cumque
-            esse nemo animi adipisci ipsum ut ad praesentium nesciunt,
-            accusantium quaerat error expedita id ipsam! Illo ut quis iusto
-            modi.
+            I was leading the creation of an educational technology platform
+            with a small bootstrapped startup. I am now looking to bring my
+            unique perspective and skillset to a collaborative environment where
+            diversity and learning is celebrated. As a Woman of Color in tech,
+            I'm passionate about supporting others, especially those from
+            underrepresented groups. My experiences with BPD and Autism have
+            helped me gain profound perspective and fathomless empathy, and I
+            manage a mental health support community called Reach. In my free
+            time, I manage a few other communities and you can also find me
+            career coaching, mentoring, pair programming, or tinkering with new
+            technology.
           </p>
         </section>
       </div>

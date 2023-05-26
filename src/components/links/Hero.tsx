c20287e0c6d9b345c8ace47c07720bd2edc6cdf3
Alexis Aguilar <alexmerona@gmail.com>
2023-05-25 21:23:23 -0400

    style: links styling

--- src/components/links/Hero.tsx
+++ src/components/links/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="py-10">
       <img
         className="m-auto mb-4 w-7/12 rounded-full sm:w-3/12 xl:w-1/5"
-        src="/assets/imgs/profile-pic.webp"
+        src="/avatar.webp"
         alt="@alexisxdawn Profile Picture | picture of alexisintech | picture of alexisxdawn | picture of Alexis Aguilar"
       />
 

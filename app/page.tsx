fe124245ce68b86a0c3e93ccce21e3b538fcb9b4
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2024-11-16 12:49:38 -0500

    delete links page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import { Before } from "./components/Before";
 import { Hero } from "./components/Hero";
 import { Now } from "./components/Now";
 import { Projects } from "./components/Projects";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Alexis Aguilar's Portfolio",
+  description:
+    "I currently work as a software engineer at Clerk. Learn a little more about me!",
+};
 
 export default function Home() {
   return (
